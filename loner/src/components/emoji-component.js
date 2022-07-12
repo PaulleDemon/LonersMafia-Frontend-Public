4d2b9d78b3ffbf0725351731571a824554240cc3
paul <paul.sajo.official@gmail.com>
2022-07-12 17:30:27 +0530

    small updates to emoji component

--- loner/src/components/emoji-component.js
+++ loner/src/components/emoji-component.js
@@ -1,13 +1,41 @@
-import myJson from './emoji-list.json'
+import { useEffect, useState } from 'react'
+import emojiJson from './emoji-list.json'
 
 
 export default function EmojiPicker(props){
 
+    const [searchValue, setSearchValue] = useState("")
+    const [emojiList, setEmojiList] = useState(Object.values(emojiJson))
+
+    useEffect(() => {
+
+        if (searchValue){
+            const new_values = Object.entries(emojiJson).filter(([k,v]) => k.startsWith(searchValue.toLowerCase())).flatMap((k,v) => k[1])
+            console.log("New values: ", new_values)
+            setEmojiList(new_values)
+        }
+
+        else{
+            setEmojiList(Object.values(emojiJson))
+        }
+
+    }, [searchValue])
+
+    const onSearchChange = (e) => {
+        console.log(e.target.value)
+        setSearchValue(e.target.value)
+    }
 
     return (
         <div className="emoji-picker">
-        
-        &#x1F600;
+            
+            <input type="search" value={searchValue} onChange={onSearchChange}/>
+
+            <div className="emoji-container">
+                {
+                    
+                }
+            </div>
             
         </div>
     )
