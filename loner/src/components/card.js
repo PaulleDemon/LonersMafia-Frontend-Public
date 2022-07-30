d081cb7af1450e274586a5d7628cb44e640e0e1e
paul <paul.sajo.official@gmail.com>
2022-07-30 12:47:13 +0530

    started with terms and conditions

--- loner/src/components/card.js
+++ loner/src/components/card.js
@@ -9,6 +9,9 @@ import ENDPOINTS from "../constants/url-endpoints"
 /**
  * name: str - name of the mafia
  * tag_line: str -tag line of the mafia
+ * icon: str- url to the icon of the mafia
+ * promoted_url: str - sponsors url
+ * promoted: bool - if its promoted indicate its promoted
  */
 
 export const MafiaCard = memo(({name="", tag_line="", icon="", 
@@ -52,6 +55,11 @@ export const MafiaCard = memo(({name="", tag_line="", icon="",
 })
 
 
+/**
+ * 
+ * message: str - the message that should be displayed on the plus icon card
+ * onClick: function -  the function that should be executed when the card is clicked
+ */
 export const CreateMafiaCard = ({message="start mafia", onClick}) => {
 
     return (
